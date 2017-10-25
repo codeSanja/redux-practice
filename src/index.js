81628cf8f75295fd9344d505b00a84389ab792ca
mandics <sanja.mandic@hmhco.com>
2017-10-25 14:32:23 +0100

    Fetching users.

--- src/index.js
+++ src/index.js
@@ -3,20 +3,41 @@ import axios from 'axios';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const reducer = (state = {}, action) => {
+const initialState = {
+  fetching: false,
+  fetched: false,
+  users: [],
+  error: null,
+};
+
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     case 'FETCH_USERS_START': {
       console.log('Working...');
+      return { ...state, fetching: true };
       break;
     }
     case 'FETCH_USERS_RECEIVED': {
-        console.log(action.payload[5]);
-        console.log(action.payload[6]);
+      console.log(action.payload[5]);
+      console.log(action.payload[6]);
+      return {
+        ...state,
+        fetching: false,
+        fetched: true,
+        users: [action.payload[5], action.payload[6]],
+      };
       break;
     }
     case 'FETCH_USERS_ERROR': {
-        console.log(action.payload);
+      return {
+        ...state,
+        fetching: false,
+        fetched: false,
+        error: action.payload,
+      };
       break;
+    }
+    default: {
     }
   }
 
